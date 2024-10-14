18ef8e16129ecc6cd65c5bb87cf6c81d751ba8e6
GMalvestiti <gustavo.smalvestiti@gmail.com>
2024-10-14 20:22:58 -0300

    Initial commit
    Signed-off-by: GMalvestiti <gustavo.smalvestiti@gmail.com>

---
+++ src/main/java/net/riser876/easychannels/EasyChannels.java
@@ -0,0 +1,10 @@
+package net.riser876.easychannels;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EasyChannels implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
